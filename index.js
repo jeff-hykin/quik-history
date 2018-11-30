0d3a65458efa577957eb062193516c42ac523284
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-11-30 16:32:23 -0600

    _init

---
+++ index.js
@@ -0,0 +1,24 @@
+let thisModule
+module.exports = thisModule = {
+    frontend : () => {
+        Object.defineProperty(history, "loadPage", {
+            get: function() { 
+                return (path) => {
+                    this.pushState({url:path},"",path)
+                    this.shallowLoad(path)
+                }
+            },
+            set: function (newValue) {
+                this.shallowLoad = newValue
+                // add the listener if it hasnt been added
+                if (!this.loadPageListenerIsSet) {
+                    this.loadPageListenerIsSet = true
+                    window.addEventListener("popstate", (e)=> {
+                        this.shallowLoad(e.state.url)
+                    })
+                }
+            }
+        })
+    },
+    generateFrontend : (server) => `${thisModule.frontend}`,
+}
